4de225c695f4da88f3c11f5c98d4d3d4f15eba3f
akashcp22 <akashcp22@gmail.com>
2025-01-31 15:50:12 +0530

    waitlistcontroller,router,waitlistmodel,questionmodel,seedQuestions

--- models/waitlistModel.js
+++ models/waitlistModel.js
@@ -6,7 +6,8 @@ const waitlistSchema = new mongoose.Schema({
   reasonOfJoin: { type: String, required: true },
   excitesOfJoin: { type: String, required: true },
   platformUse: { type: String, required: true },  // Example: "Web, Mobile"
-  infoOfPlatform: { type: String, required: true } // Example: "Social media, Friends, Ads"
+  infoOfPlatform: { type: String, required: true } ,
+  points: { type: Number, required: true }// Example: "Social media, Friends, Ads"
 }, { timestamps: true });
 
 module.exports = mongoose.model('Waitlist', waitlistSchema);
--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,16 @@
 const express = require('express')
 //import user controller
 const usercontroller =  require('../Controller/userController')
-
+const waitlistController=  require('../Controller/waitlistController')
 //create instance router
 const router = new express.Router()
 
 //REGISTER
 router.post('/register',usercontroller.register)
+
+router.post('/waitlist',waitlistController.addToWaitlist)
+
+router.get('/stats', waitlistController.getWaitlistStats); 
 
 
 module.exports = router
