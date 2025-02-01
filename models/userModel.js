fdedaa9762945d42873474e345cb192a1ed720a8
ASWANTHbb <aswanthbbofficial@gmail.com>
2025-02-01 15:27:15 +0530

    package & usermodel

--- models/userModel.js
+++ models/userModel.js
@@ -17,15 +17,6 @@ const userSchema = new mongoose.Schema({
         type:String
     }
 
-   username: {
-    type:String,
-    required:true,
-    unique:true
-   },
-   password:{
-    type:String,
-    required:true
-   }
 
 })
 
